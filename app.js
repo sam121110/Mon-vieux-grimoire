0eeb781192b1fe9d5a2d431eb4015a572502b1eb
X738173 <stephane.le-minh-nhut@socgen.com>
2024-06-09 20:27:50 +0200

    configuration base de données

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-
+const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://sam121110987654321:<password>@cluster16.vssydqk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster16',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
